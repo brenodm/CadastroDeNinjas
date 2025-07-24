ed4cfa339ce83b2cf2d81ba92e1c5030d08c53b5
brenodm <breno.maciel1337@gmail.com>
2025-07-23 22:29:18 -0400

    fix: Adiciona o nome das Column na minha classe

--- src/main/java/dev/Java10x/CadastroDeNinjas/Ninjas/NinjaModel.java
+++ src/main/java/dev/Java10x/CadastroDeNinjas/Ninjas/NinjaModel.java
@@ -17,47 +17,21 @@ public class NinjaModel {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column (name= "id")
     long id;
+
+    @Column (name= "nome")
     private String nome;
+
+    @Column (name= "email")
     private String email;
+
+    @Column (name= "idade")
     private int idade;
 
     @ManyToOne
     @JoinColumn(name = "missoes_id")
     private MissoesModel missoes;
 
-    public NinjaModel(String nome) {
-        this.nome = nome;
-    }
-
-    public NinjaModel(String nome, String email, int idade) {
-        this.nome = nome;
-        this.email = email;
-        this.idade = idade;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public int getIdade() {
-        return idade;
-    }
-
-    public void setIdade(int idade) {
-        this.idade = idade;
-    }
 
 }
