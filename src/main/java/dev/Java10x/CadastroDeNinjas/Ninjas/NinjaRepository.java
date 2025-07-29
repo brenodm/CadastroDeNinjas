20d2de57c0b43e7b2cd9e9b79ecde364116b2fae
brenodm <breno.maciel1337@gmail.com>
2025-07-28 20:45:15 -0400

    fix: altera o NinjaRepository para uma interface e extende o JpaRepository

--- src/main/java/dev/Java10x/CadastroDeNinjas/Ninjas/NinjaRepository.java
+++ src/main/java/dev/Java10x/CadastroDeNinjas/Ninjas/NinjaRepository.java
@@ -1,7 +1,8 @@
 package dev.Java10x.CadastroDeNinjas.Ninjas;
 
-public class NinjaRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
 
+public interface NinjaRepository extends JpaRepository<NinjaModel, Long> {
 
 
 }
